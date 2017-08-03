0bf127e27837fe1ee12e008e27e9b08467d45b83
Rob Latimer <rtlatimer@gmail.com>
2017-08-02 22:22:57 -0700

    refactor: implement faster solution for question 1

--- solutions.py
+++ solutions.py
@@ -4,6 +4,8 @@ Given two strings s and t, determine whether some anagram of t is a substring of
 For example: if s = "udacity" and t = "ad", then the function returns True. 
 Your function definition should look like: question1(s, t) and return a boolean True or False.'''
 
+'''
+*** FIRST SUBMISSION *** time complexity == O(n^2)
 def question1(s,t):
     if s and t: # Check to see if string is empty
         if len(s) >= len(t): # 't' should not be longer than 's'
@@ -13,28 +15,56 @@ def question1(s,t):
                     return True
     print "Looking for '" + t + "' in '" + s + "': "
     return False
+'''
+
+# *** SECOND SUBMISSION *** time comlexity == O(n)
+from collections import Counter
+
+def question1(s,t):
+    if s is None or t is None: # check for null inputs
+    	print "Looking for '" + str(t) + "' in '" + str(s) + "': "
+    	return "Inputs cannot be null."
+    elif s != str(s) or t != str(t): # inputs must be strings
+        print "Looking for '" + str(t) + "' in '" + str(s) + "': "
+        return "Inputs must be strings containing only letters."
+    elif len(s) == 0 or len(t) == 0 or len(t) >= len(s): # 's' must be longer than 't'. Inputs can't be empty.
+        print "Looking for '" + t + "' in '" + s + "': "
+        return "Inputs cannot be empty. 's' must be longer than 't'."
+    s,t = s.lower(), t.lower() # correct for upper + lower case letters
+    t_dict = Counter(t) # create dictionary of letters in 't'
+    for i in range(len(s) - len(t) + 1): # loop over 's' to find all possible substrings of len(t)
+        s_slice = Counter(s[i:i+len(t)]) # convert 's' substring to dict to compare it to 't_dict'
+        if s_slice == t_dict: # True if one of the 's_slice' equals 't_dict'
+            print "Looking for '" + t + "' in '" + s + "': "
+            return True
+    print "Looking for '" + t + "' in '" + s + "': "
+    return False
 
 
 print "*** Question 1 ***"
 print ""
 print question1('udacity', 'ad') 
 # Answer: True
-print question1('udaCiTy', 'city') 
-# Answer: True
-# Edge Case: upper and lower-case letters
 print question1('dormitory', 'room') 
 # Answer: False
 print question1('astronomy', 'moony') 
 # Answer: True
 print question1('bubble','sort') 
 # Answer: False
-print question1('bubble','') 
-# Answer: False
-# Edge Case: Empty input
 print question1('bubble','bub') 
 # Answer: True
-print question1('spongebob', 'bboeg') 
+
+# ***Edge Cases***
+print question1('udaCiTy', 'cIty') # Edge Case: upper and lower-case letters
 # Answer: True
+print question1('bubble','') # Edge Case: Empty string
+# Answer: Inputs cannot be empty. 's' must be longer than 't'.
+print question1('bubble', None) # Edge Case: Null input
+# Answer: Inputs cannot be null.
+print question1(1,'abc') # Edge Case: Integer input
+# Answer: Inputs must be strings containing only letters.
+print question1('abc','cba') # Edge Case: 's' and 't' are the same length.
+# Answer: Inputs must be strings containing only letters.
 print ""
 
 
@@ -60,10 +90,10 @@ def question2(a):
             return word
         else:
             print "Looking for palindrome in '" + a + "':"
-            return None
+            return
     print "Looking for palindrome in '" + a + "':"
     print "Word not long enough to test."
-    return None
+    return
     
 print "*** Question 2 ***"
 print ""
@@ -401,7 +431,7 @@ def question5(ll, m):
         if current_node and ll_count > 0: # Make sure values are positive
             return current_node.data # Once you get to m, return value
         else:
-            return None
+            return
 
 n1 = Node(1)
 n2 = Node(2)
